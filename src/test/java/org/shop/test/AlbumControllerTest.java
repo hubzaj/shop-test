8a57b5980db6bd507e9134291314f6a978ee163b
Hubert Zajac <hubert.zajac@openx.com>
2023-12-23 15:37:39 +0100

    test

--- src/test/java/org/shop/test/AlbumControllerTest.java
+++ src/test/java/org/shop/test/AlbumControllerTest.java
@@ -1,13 +1,30 @@
 package org.shop.test;
 
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 import org.shop.BaseTest;
+
+import static org.hamcrest.Matchers.equalTo;
 
 public class AlbumControllerTest extends BaseTest {
 
     @Test
     void createNewAlbumTest() {
-        System.out.println("asda");
+        // Given
+        RequestSpecification httpRequest = RestAssured.given();
+
+        // When
+        Response response = httpRequest.get("health");
+
+        // Then
+        response.then()
+                .log()
+                .ifValidationFails(LogDetail.BODY)
+                .statusCode(200)
+                .body(equalTo("\"Hi! I am alive!\""));
     }
 
 }
