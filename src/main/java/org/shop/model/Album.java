42638a98783f106d4b0253e301791ca020bc847d
Hubert Zajac <hubert.zajac@openx.com>
2023-12-27 12:34:54 +0100

    album test fix

--- src/main/java/org/shop/model/Album.java
+++ src/main/java/org/shop/model/Album.java
@@ -1,19 +1,30 @@
 package org.shop.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import java.util.UUID;
 
+@Builder
+@Data
 @Setter
 @Getter
 @ToString
-@Builder
 public class Album {
     UUID id;
     String title;
     String artist;
     float price;
+
+    @JsonCreator
+    public Album(@JsonProperty("id") UUID id,
+                 @JsonProperty("title") String title,
+                 @JsonProperty("artist") String artist,
+                 @JsonProperty("price") float price) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.price = price;
+    }
 }
