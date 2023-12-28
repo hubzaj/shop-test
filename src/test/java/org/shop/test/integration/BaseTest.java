9fe9be4b0e928e1be3a36f7a95dd42a65a4c9a17
Hubert Zajac <hubert.zajac@openx.com>
2023-12-28 14:09:17 +0100

    on-demand suffix support and simplification

--- src/test/java/org/shop/test/integration/BaseTest.java
+++ src/test/java/org/shop/test/integration/BaseTest.java
@@ -2,13 +2,13 @@ package org.shop.test.integration;
 
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.shop.config.RestAssuredConfig;
+import org.shop.extension.RestAssuredExtension;
 
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @TestInstance(PER_CLASS)
 @ExtendWith({
-        RestAssuredConfig.class
+        RestAssuredExtension.class
 })
 public class BaseTest {
 
